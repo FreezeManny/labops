72ca51bb303aa0dfdafabe5ce74f90b430adbd37
Manuel Fritz <freezemanny@gmail.com>
2026-04-22 12:59:50 +0000

    feat: added docker stack duplicate name validator

--- models/input_conf/docker.py
+++ models/input_conf/docker.py
@@ -4,10 +4,30 @@ from typing import Optional, Dict
 from .web_services import WebServices
 from .custom_types import StrictModel
 
+
+class StackEntry(StrictModel):
+    name: str = ""
+    config_path: DirectoryPath
+    web_services: Optional[WebServices] = None
+
+
 class Docker(StrictModel):
     root_path: str
     stacks: Dict[str, StackEntry]
-    
-class StackEntry(StrictModel):
-    config_path: DirectoryPath
-    web_services: Optional[WebServices] = None
+
+    @model_validator(mode="after")
+    def propagate_stack_names(self) -> "Docker":
+        for k, stack in self.stacks.items():
+            stack.name = k
+        return self
+
+    @model_validator(mode="after")
+    def validate_unique_stack_name(self) -> "Docker":
+        all_names: set[str] = set()
+        for name in self.stacks.keys():
+            if name in all_names:
+                raise ValueError(f"Duplicate stack name: '{name}'")
+            all_names.add(name)
+        return self
+
+
