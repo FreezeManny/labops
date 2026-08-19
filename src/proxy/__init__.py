648401efa8dd4c15a80ec0e4de1addedfb26b420
Manuel Fritz <freezemanny@gmail.com>
2026-08-19 16:11:55 +0200

    added function

--- src/proxy/__init__.py
+++ src/proxy/__init__.py
@@ -1,10 +1,11 @@
 from .find import find_routes
-from .render import render_caddyfile, tls_warnings
+from .render import render_caddyfile, resolve_acl, tls_warnings
 from .deploy import sync_proxy, deploy_proxy, reload_proxy
 
 __all__ = [
     "find_routes",
     "render_caddyfile",
+    "resolve_acl",
     "tls_warnings",
     "sync_proxy",
     "deploy_proxy",
